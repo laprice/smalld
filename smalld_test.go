29347950fd13ca3e76b509727f114b7ccf187a13
Larry Price <larry@industrialintellect.com>
2014-06-07 10:46:57 -0700

    updated to have own db connection for testing

--- smalld_test.go
+++ smalld_test.go
@@ -1,9 +1,14 @@
 package main
 
+import "os"
 import "log"
 import "testing"
 import "net/http"
 import "net/http/httptest"
+import ( 
+	_ "github.com/lib/pq"
+	"database/sql"
+)
 
 func TestLocationHandlerResponseOK(t *testing.T) {
 	response := httptest.NewRecorder()
@@ -13,15 +18,31 @@ func TestLocationHandlerResponseOK(t *testing.T) {
 	if response.Code != http.StatusNoContent {
 		t.Fatalf("Bad Response")
 	}
-
 }
 
-func TestLocationHandlerQueryParse(t *testing.T) {
+func TestLocationHandlerResponseQuery(t *testing.T) {
 	response := httptest.NewRecorder()
-	request, _ := http.NewRequest("GET", "/location?lat=44.09491559960329&lon=-123.0965916720434&acc=5&label=foo", nil)
+	request, _ := http.NewRequest("GET", "http://localhost:8000/location?lat=44.09491559960329&lon=-123.0965916720434&acc=5&label=foo", nil)
 	LocationHandler(response, request)
 	if response.Code != http.StatusOK {
 		t.Fatalf("Bad Response")
 	}
 	log.Println(response)
+}
+
+func init() {
+	log.Println("smalld testing")
+	db_connection := os.Getenv("SMALLD_DB_CONNECTION")
+	url_base := os.Getenv("SMALLD_URL_BASE")
+	options := os.Getenv("SMALLD_OPTIONS") //override command line flags 
+	log.Println("SMALLD_DB_CONNECTION:", db_connection)
+	log.Println("SMALLD_URL_BASE:", url_base)
+	log.Println("SMALLD_OPTIONS:", options)
+	var err error
+	db, err = sql.Open("postgres", db_connection)
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	} 
+	log.Println("connected to database")
 }
