f64015c0e3cce253b598ce02d1f9918651120740
Larry Price <larry@industrialintellect.com>
2015-02-02 18:30:25 -0800

    moved database write to goroutine

--- smalld.go
+++ smalld.go
@@ -4,9 +4,9 @@ import "os"
 import "log"
 import "fmt"
 import "strconv"
-import ( 
-	_ "github.com/lib/pq"
+import (
 	"database/sql"
+	_ "github.com/lib/pq"
 )
 import "net/http"
 import "net/url"
@@ -19,27 +19,49 @@ func SafeValues(v *url.Values) bool {
 	return true //for now
 }
 
-func makePoint(v *url.Values) (string) {
+func makePoint(v *url.Values) string {
 	point := fmt.Sprintf("POINT(%s %s)", v.Get("lon"), v.Get("lat"))
 	return point
+}
+
+func recordlocations(v *url.Values) {
+	p := makePoint(v)
+	txn, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	label := fmt.Sprintf("%s", v.Get("label"))
+	acc, err := strconv.ParseFloat(v.Get("acc"), 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Query("insert into locations ( label, acc, geom ) values ( $1, $2, ST_PointFromText( $3, 4326) )", label, acc, p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = txn.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func LocationHandler(w http.ResponseWriter, req *http.Request) {
 	log.Println("handling url", req.URL)
 	if req.Method == "GET" {
 		if req.URL.RawQuery != "" {
-		values, err := url.ParseQuery(req.URL.RawQuery)
+			values, err := url.ParseQuery(req.URL.RawQuery)
 			if err != nil {
 				log.Fatal(err)
 			}
 			log.Println(values)
 			if SafeValues(&values) {
 				p := makePoint(&values)
+				go recordlocations(&values)
 				log.Println("point:", p)
 				q := "select name from adminareas where st_contains(adminareas.geom, st_geomfromtext( $1 , 4326))"
-				rows, err := db.Query(q, p )
+				rows, err := db.Query(q, p)
 				if err != nil {
-					log.Print("db error",err)
+					log.Print("db error", err)
 				}
 				var l []string
 				for rows.Next() {
@@ -55,30 +77,13 @@ func LocationHandler(w http.ResponseWriter, req *http.Request) {
 					log.Fatal(err)
 				}
 				w.Write(j)
-				txn, err := db.Begin()
-				if err != nil {
-					log.Fatal(err)
-				}
-				label := fmt.Sprintf("%s", values.Get("label"))
-				acc, err := strconv.ParseInt(values.Get("acc"), 10, 0)
-				if err != nil {
-					log.Fatal(err)
-				}
-				rows, err = db.Query( "insert into locations ( label, acc, geom ) values ( $1, $2, ST_PointFromText( $3, 4326) )", label, acc, p )
-				if err != nil {
-					log.Fatal(err)
-				}
-				err = txn.Commit()
-				if err != nil {
-					log.Fatal(err)
-				}
 				return
 			}
 		} else {
 			http.Error(w, "No Content", http.StatusNoContent)
 			return
 		}
-		
+
 	}
 }
 
@@ -86,7 +91,7 @@ func main() {
 	log.Println("smalld starting")
 	db_connection := os.Getenv("SMALLD_DB_CONNECTION")
 	url_base := os.Getenv("SMALLD_URL_BASE")
-	options := os.Getenv("SMALLD_OPTIONS") //override command line flags 
+	options := os.Getenv("SMALLD_OPTIONS") //override command line flags
 	log.Println("SMALLD_DB_CONNECTION:", db_connection)
 	log.Println("SMALLD_URL_BASE:", url_base)
 	log.Println("SMALLD_OPTIONS:", options)
@@ -95,7 +100,7 @@ func main() {
 	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
-	} 
+	}
 	log.Println("connected to database")
 	http.HandleFunc("/location", LocationHandler)
 	log.Println("registered LocationHandler")
