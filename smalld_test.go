b26b055404a30644c5e327cacfcac2d10cbc9f6a
Larry Price <larry@industrialintellect.com>
2015-02-11 17:21:09 -0800

    updated test for recording location. Don't trust it. If what I think is happening is the case it will die on CI box.

--- smalld_test.go
+++ smalld_test.go
@@ -31,15 +31,24 @@ func TestLocationHandlerResponseQuery(t *testing.T) {
 }
 
 func TestLocationHandlerrecordlocations(t *testing.T) {
-	query := "select label, acc, st_y(geom) lat, st_x(geom) lon from locations where label='foo'"
-	result, err := db.Query(query)
+	query := "select label, acc, st_y(geom) lat, st_x(geom) lon from locations where label='foo' limit 1"
+	var result Location
+	err := db.QueryRow(query).Scan(&result.label, &result.acc,&result.lat,&result.lon)
 	if err != nil {
+		log.Println(err)
 		t.Fatalf("could not talk to database")
 	}
-	if result.label != 'foo' { t.Fatalf("label inserted does not match") }
+	if result.label != "foo" { t.Fatalf("label inserted does not match") }
 	if result.acc != 5 { t.Fatalf("acc inserted does not match") }
 	log.Println(result)
 }
+
+type Location struct {
+	label string
+	lat float64
+	lon float64
+	acc float64
+	}
 
 func init() {
 	log.Println("smalld testing")
